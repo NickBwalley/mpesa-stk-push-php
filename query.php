4064486597007b64681d583da1b223f258465ad7
NickBwalley <nickbwalley@gmail.com>
2023-11-09 02:12:43 +0300

    lastcommit 202311090212

--- query.php
+++ query.php
@@ -9,7 +9,7 @@ $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 // ENCRIPT  DATA TO GET PASSWORD
 $Password = base64_encode($BusinessShortCode . $passkey . $Timestamp);
 //THIS IS THE UNIQUE ID THAT WAS GENERATED WHEN STK REQUEST INITIATED SUCCESSFULLY
-$CheckoutRequestID = 'ws_CO_09112023010329954714394332';
+$CheckoutRequestID = 'ws_CO_09112023010850890714394332';
 $queryheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
 # initiating the transaction
 $curl = curl_init();
