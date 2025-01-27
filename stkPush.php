b130d524d7f448eeb76b81fb970ec874dba3521d
NickBwalley <nickbwalley@gmail.com>
2025-01-27 21:25:37 +0300

    Update stkPush.php

--- stkPush.php
+++ stkPush.php
@@ -59,7 +59,7 @@
     date_default_timezone_set('Africa/Nairobi');
     $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
     $callbackurl = 'https://f833-197-237-166-21.ngrok-free.app/callback.php';
-    $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+    $passkey = "";
     $BusinessShortCode = '174379';
     $Timestamp = date('YmdHis');
     // ENCRIPT  DATA TO GET PASSWORD
